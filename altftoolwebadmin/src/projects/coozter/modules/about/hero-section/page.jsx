d80f3b83e9a7ffb5ca136d510c634d4f7346b723
Nidhi Maurya <nidhi.maurya@anslation.com>
2026-07-09 19:00:39 +0530

    about file

--- altftoolwebadmin/src/projects/coozter/modules/about/hero-section/page.jsx
+++
@@ -1,5 +0,0 @@
-import { SectionEditor } from "../components/AboutSectionShared";
-
-export default function HeroSectionTab(props) {
-  return <SectionEditor sectionKey="heroSection" {...props} />;
-}
--- altftoolwebadmin/src/projects/coozter/modules/about/proof-metrics/page.jsx
+++
@@ -1,5 +0,0 @@
-import { SectionEditor } from "../components/AboutSectionShared";
-
-export default function ProofMetricsTab(props) {
-  return <SectionEditor sectionKey="proofMetrics" {...props} />;
-}
